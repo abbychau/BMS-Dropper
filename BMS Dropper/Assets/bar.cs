abde9a80c59e272300fa7a86ca560b025174e78e
abbychau <abbychau@gmail.com>
2015-10-02 00:37:58 +0800

    init files

---
+++ BMS Dropper/Assets/bar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bar : MonoBehaviour {
+
+    public Object flare;
+    void Start()
+    {
+        flare = Resources.Load("Flares/FlareCore-Autumn");
+        //flare
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Object newFlare = Instantiate(flare, collision.transform.position, Quaternion.identity);
+        //newFlare.Play();
+        Destroy(newFlare, 0.3f);
+    }
+}
