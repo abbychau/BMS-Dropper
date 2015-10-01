b772dc804046918a88fbcc13616908941b92593c
abbychau <abbychau@gmail.com>
2015-10-02 00:49:58 +0800

    Revert "a"
    This reverts commit 8061ee9105841c19ba86143c0750ff919a4c9ee1.

---
+++ BMS Dropper/Assets/DeadLine.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadLine : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
